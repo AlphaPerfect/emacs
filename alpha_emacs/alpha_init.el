0f96dacd3d672b83dba7f73674543c413dff08ee
lamentedward <lamentedward@gmail.com>
2013-08-30 00:49:14 +0800

    change the init directory to github/emacs

--- alpha_emacs/alpha_init.el
+++ alpha_emacs/alpha_init.el
@@ -2,19 +2,19 @@
 ;; .emacs for ubuntu
 ;; usage: sh auto_install.sh
 
-(if (file-exists-p "~/work/ggcode/elisp/alpha_emacs/innate/basic.el") 
-    (load "~/work/ggcode/elisp/alpha_emacs/innate/basic.el"))
+(if (file-exists-p "~/work/github/emacs/alpha_emacs/innate/basic.el") 
+    (load "~/work/github/emacs/alpha_emacs/innate/basic.el"))
 
 ;; init the emacs key binding if the file exist that setting the key mapping
-(if (file-exists-p "~/work/ggcode/elisp/alpha_emacs/innate/key_binding.el") 
-    (load "~/work/ggcode/elisp/alpha_emacs/innate/key_binding.el"))
+(if (file-exists-p "~/work/github/emacs/alpha_emacs/innate/key_binding.el") 
+    (load "~/work/github/emacs/alpha_emacs/innate/key_binding.el"))
 
 
 ;;
 ;; add load path which has the elisp file to config my emacs
 ;; if the alpha_emacs 
-(add-to-list 'load-path "~/work/ggcode/elisp/alpha_emacs/")
-(let ((default-directory "~/work/ggcode/elisp/alpha_emacs"))
+(add-to-list 'load-path "~/work/github/emacs/alpha_emacs/")
+(let ((default-directory "~/work/github/emacs/alpha_emacs"))
      (normal-top-level-add-subdirs-to-load-path))
 ;;
 ;; Load the extension configration of emacs
