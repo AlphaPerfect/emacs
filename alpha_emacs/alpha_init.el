2220ad625e4570d0400ce79b93349fcf61073bc2
Alpha Perfect <AlphaPerfect@gmail.com>
2014-11-27 18:14:28 +0800

    add color theme config

--- alpha_emacs/alpha_init.el
+++ alpha_emacs/alpha_init.el
@@ -1,18 +1,17 @@
-
 ;; .emacs for ubuntu
 ;; usage: sh auto_install.sh
 
-(if (file-exists-p "~/work/github/emacs/alpha_emacs/innate/basic.el") 
+(if (file-exists-p "~/work/github/emacs/alpha_emacs/innate/basic.el")
     (load "~/work/github/emacs/alpha_emacs/innate/basic.el"))
 
 ;; init the emacs key binding if the file exist that setting the key mapping
-(if (file-exists-p "~/work/github/emacs/alpha_emacs/innate/key_binding.el") 
+(if (file-exists-p "~/work/github/emacs/alpha_emacs/innate/key_binding.el")
     (load "~/work/github/emacs/alpha_emacs/innate/key_binding.el"))
 
 
 ;;
 ;; add load path which has the elisp file to config my emacs
-;; if the alpha_emacs 
+;; if the alpha_emacs
 (add-to-list 'load-path "~/work/github/emacs/alpha_emacs/")
 (let ((default-directory "~/work/github/emacs/alpha_emacs"))
      (normal-top-level-add-subdirs-to-load-path))
@@ -20,8 +19,9 @@
 ;; Load the extension configration of emacs
 (require 'ext_emacs)
 
+;; load the theme
+(require 'alpha_theme)
 
 ;;set the chinese environment
 ;(set-language-environment 'Chinese-GBK)
 ;(prefer-coding-system 'utf-8)
-
