a90ab15039a600a3e81148e60df775b36b452493
Alpha Perfect <AlphaPerfect@gmail.com>
2014-11-27 18:51:45 +0800

    add zenburn theme

--- alpha_emacs/theme/alpha_theme.el
+++ alpha_emacs/theme/alpha_theme.el
@@ -1,7 +1,8 @@
 (provide 'alpha_theme)
 
 (add-to-list 'custom-theme-load-path "~/work/github/emacs/alpha_emacs/theme/")
-(load-theme 'molokai t)
+(load-theme 'zenburn t)
+;(load-theme 'molokai t)
 ;(load-theme 'misterioso)
 ;(load-theme 'tango-dark)
 ;(load-theme 'adwaita t)
