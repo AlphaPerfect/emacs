d5c31c61adbb2de2ddc46b648f58014ca8600182
alphaperfect <alphaperfect@gmail.com>
2014-11-27 15:19:24 +0800

    add gocode install methord, change the auto-complet and go-autocomplete load sequence

--- alpha_emacs/extensions/auto_complete/auto_complete_conf.el
+++ alpha_emacs/extensions/auto_complete/auto_complete_conf.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
 ;(require 'auto-complete-autoloads)
-(require 'go-autocomplete)
 (require 'auto-complete-config)
+(require 'go-autocomplete)
+
 ;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
