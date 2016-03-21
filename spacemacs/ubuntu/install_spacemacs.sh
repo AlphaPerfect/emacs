1f5dbf5524a03e818c439865068fbcf397233636
alphaperfect <alphaperfect@gmail.com>
2016-03-21 18:24:24 +0800

    fix:wrong config dir

--- spacemacs/ubuntu/install_spacemacs.sh
+++ spacemacs/ubuntu/install_spacemacs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 GITHUB_DIR=$HOME/work/github
+SPACEMACS_CONFIG_DIR=$GITHUB_DIR/emacs/spacemacs/ubuntu
 
 if [ -f $GITHUB_DIR/spacemacs ]; then
     echo "the $SPACEMACS_DIR has exist"
@@ -13,8 +14,8 @@ fi
 if [ -f ~/.spacemacs ]; then
     unlink ~/.spacemacs 
     echo "unlink the old link file ~/.spacemacs"
-    ln -s ${PWD}/spacemacs.ubuntu ~/.spacemacs
+    ln -s ${SPACEMACS_CONFIG_DIR}/spacemacs.ubuntu ~/.spacemacs
 else
     echo "add soft link from ${PWD}/spacemacs to ~/.spacemacs"
-    ln -s ${PWD}/spacemacs.ubuntu ~/.spacemacs
+    ln -s ${SPACEMACS_CONFIG_DIR=}/spacemacs.ubuntu ~/.spacemacs
 fi
